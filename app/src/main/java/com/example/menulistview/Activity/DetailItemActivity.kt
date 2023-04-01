bb3c11a1df422b27435c10a5f17bdc4ad69855e1
Dariusz Marciniak <d.marciniak@insys.pl>
2023-04-01 10:17:35 +0200

    add detail fragment connection to Activity Detail

--- app/src/main/java/com/example/menulistview/Activity/DetailItemActivity.kt
+++ app/src/main/java/com/example/menulistview/Activity/DetailItemActivity.kt
@@ -3,10 +3,16 @@ package com.example.menulistview.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.menulistview.R
+import com.example.menulistview.RecipeDetailFragment
 
 class DetailItemActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_item)
+        (supportFragmentManager.findFragmentById(R.id.detail_frag) as RecipeDetailFragment?)?.setRecipe(
+            1
+        )
+
     }
 }
--- app/src/main/java/com/example/menulistview/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/menulistview/RecipeDetailFragment.kt
@@ -7,8 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 
 class RecipeDetailFragment : Fragment() {
+
+    private var recipeId: Long = 0
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_recipe_detail, container, false)
+
     }
+    fun setRecipe(id: Long) {
+        this.recipeId = id
+    }
+
 
 }
