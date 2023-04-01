37c56af83badb429ab8be39f9e80745c10d080e0
Michał Dwernicki <dwernicki.michal@gmail.com>
2023-04-01 10:08:20 +0200

    Dodanie Dish class

--- app/src/main/java/com/example/menulistview/Activity/DetailItemActivity.kt
+++ app/src/main/java/com/example/menulistview/Activity/DetailItemActivity.kt
@@ -1,12 +1,17 @@
 package com.example.menulistview.Activity
 
+import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.menulistview.R
+import com.example.menulistview.RecipeDetailFragment
+
 
 class DetailItemActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_item)
+        setContentView(com.example.menulistview.R.layout.activity_detail_item)
+        val frag: RecipeDetailFragment? =
+            supportFragmentManager.findFragmentById(com.example.menulistview.R.id.detail_frag) as RecipeDetailFragment?
+        frag.setCocktail(1)
     }
 }
